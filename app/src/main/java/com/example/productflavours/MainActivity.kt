c121fc624f32a193784140e2869c14444a90bf7a
Prajwal W <prajwalwaingankar@yahoo.in>
2024-09-16 22:02:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/productflavours/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.productflavours
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btn: Button = findViewById(R.id.btn)
+        btn.setOnClickListener(View.OnClickListener {
+
+            if (BuildConfig.FLAVOR == BuildConfig.FLAVOR_version) {
+
+            }
+            val intent = Intent(this, CustomActivity::class.java)
+            startActivity(intent)
+        })
+
+    }
+}
